2c2eec5d89c6ed2eafafc97a05397def467af4df
oconn <matthew.j.oconnell1@gmail.com>
2014-02-26 11:24:07 -0600

    added routes

---
+++ app/controllers/comment.rb
@@ -0,0 +1,15 @@
+get '/comment/new' do
+
+end
+
+get 'comment/edit' do
+
+end
+
+get '/comment/:id' do
+
+end
+
+post 'comment/delete/:id' do
+
+end
--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -2,3 +2,14 @@ get '/post/new' do
 
 end
 
+get 'post/edit' do
+
+end
+
+get '/post/:id' do
+
+end
+
+post 'post/delete/:id' do
+
+end
